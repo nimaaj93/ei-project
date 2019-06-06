887d575110d1c41d3fbbf44a31b9a6682f322af9
nima.aj93 <nima.aj93@gmail.com>
2019-06-06 18:15:34 +0430

    Fixed api security

--- src/main/java/com/ashkan/ie/config/SecurityConfig.java
+++ src/main/java/com/ashkan/ie/config/SecurityConfig.java
@@ -97,7 +97,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-//                .antMatchers("/api/v1/**").authenticated()
+                .antMatchers("/api/v1/user/profile").authenticated()
                 .antMatchers("/api/v1/user/authenticate").permitAll()
                 .antMatchers("/api/v1/user/register").permitAll()
                 .and()
